b9b536769c0a6b0027944e7f89d89b0b963633fe
root <root@hisrp2yi.my.domain>
2017-07-06 15:39:05 +0200

    automaticaly generate config files and files are stored on a volume persistent

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,13 @@ RUN apk add --no-cache libgcc libstdc++ fuse
 RUN mkdir -p /usr/local/etc/mfs/ && mkdir -p /usr/local/var/lib/mfs && chown mfs:mfs /usr/local/var/lib/mfs && mkdir -p /usr/local/var/lib/mfs/
 COPY mfsmaster /usr/local/bin/mfsmaster
 COPY mfsmetarestore /usr/local/bin/mfsmetarestore
-COPY mfsmaster.cfg /usr/local/etc/mfs/mfsmaster.cfg
-COPY mfsexports.cfg /usr/local/etc/mfs/mfsexports.cfg
+COPY mfsmaster.cfg /usr/local/mfsmaster.cfg
+COPY mfsexports.cfg /usr/local/mfsexports.cfg
+COPY mfsgoals.cfg /usr/local/mfsgoals.cfg
+COPY mfstopology.cfg /usr/local/mfstopology.cfg
 COPY metadata.mfs.empty /usr/local/metadata.mfs.empty
 VOLUME /usr/local/var/lib/mfs
+VOLUME /usr/local/etc/
 
 ENV MFSM_PERSONALITY master
 ENV MFSM_MASTERHOST mfsmaster
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,44 @@
 #!/bin/sh
+
+mkdir -p /usr/local/etc/mfs/
+chown mfs:mfs -R /usr/local/
+
+for configFiles in $(ls /usr/local/*.cfg)
+do
+        if [ ! -f /usr/local/etc/mfs/mfsmaster.cfg ]
+        then
+                echo "copy initial $configFiles"
+                cp /usr/local/$(basename "$configFiles") /usr/local/etc/mfs/
+        fi
+done
 MASTER_CFG=/usr/local/etc/mfs/mfsmaster.cfg
 
-chown mfs:mfs -R /usr/local/var/lib/mfs
 echo "Setup mfsmaster as $MFSM_PERSONALITY"
-sed -i "s/# PERSONALITY = master/PERSONALITY = $MFSM_PERSONALITY/g" $MASTER_CFG
-sed -i "s/# NO_ATIME = 0/NO_ATIME = 1/g" $MASTER_CFG
+sed -i "s/^PERSONALITY = .*\|# PERSONALITY = master/PERSONALITY = $MFSM_PERSONALITY/g" $MASTER_CFG
+sed -i "s/^NO_ATIME = .\|# NO_ATIME = 0/NO_ATIME = 1/g" $MASTER_CFG
 echo "Set master host as $MFSM_MASTERHOST"
-sed -i "s/# MASTER_HOST = mfsmaster/MASTER_HOST = $MFSM_MASTERHOST/g" $MASTER_CFG
-sed -i "s/# CHUNKS_REBALANCING_BETWEEN_LABELS = 0/CHUNKS_REBALANCING_BETWEEN_LABELS = $MFSM_REBAL_LABELS/g" $MASTER_CFG
+sed -i "s/^MASTER_HOST = .*\|# MASTER_HOST = mfsmaster/MASTER_HOST = $MFSM_MASTERHOST/g" $MASTER_CFG
+sed -i "s/^CHUNKS_REBALANCING_BETWEEN_LABELS = .\|# CHUNKS_REBALANCING_BETWEEN_LABELS = ./CHUNKS_REBALANCING_BETWEEN_LABELS = $MFSM_REBAL_LABELS/g" $MASTER_CFG
 echo "Set acceptable difference to $MFSM_ACCEPT_DIFF"
-sed -i "s/# ACCEPTABLE_DIFFERENCE = 0.1/ACCEPTABLE_DIFFERENCE = $MFSM_ACCEPT_DIFF/g" $MASTER_CFG
+sed -i "s/^ACCEPTABLE_DIFFERENCE = ...\|^#.ACCEPTABLE_DIFFERENCE = .../ACCEPTABLE_DIFFERENCE = $MFSM_ACCEPT_DIFF/g" $MASTER_CFG
 
-if [ -f /usr/local/var/lib/mfs/metadata.mfs ]
+for configFiles in $(ls /usr/local/*.cfg)
+do
+	if [ ! -f /usr/local/etc/mfs/mfsmaster.cfg ]
+	then
+		echo "copy initial $configFiles"
+		cp /usr/local/$(basename "$configFiles") /usr/local/etc/mfs/
+	fi
+done
+
+
+if [ ! -f /usr/local/var/lib/mfs/metadata.mfs ]
 then
-	echo "mfsmetadata exists, start mfsmaster"
-else
 	echo "mfsmetadata does not exist, copy empty one"
 	cp /usr/local/metadata.mfs.empty /usr/local/var/lib/mfs/metadata.mfs
 	echo "start mfsmaster"
 fi
+
 if [ -f /usr/local/var/lib/mfs/metadata.mfs.lock ]
 then
 	echo "lockfile found, run mfsmetarestore"
